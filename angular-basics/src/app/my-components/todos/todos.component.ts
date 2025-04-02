c775c40609a16720686df602988ccd4b7a865959
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-04-02 16:41:41 +0000

    adjustments commit

--- angular-basics/src/app/my-components/todos/todos.component.ts
+++ angular-basics/src/app/my-components/todos/todos.component.ts
@@ -41,9 +41,7 @@ export class TodosComponent implements OnInit {
       if (this.localTodos != null) {
         this.todos = JSON.parse(this.localTodos);
       }
-      else {
-        
-      }
+      
     }
   }
 
